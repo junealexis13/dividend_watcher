8fea7d84159bdd504f31244e1dd05c7debcc7db4
junealexis13 <junealexis.santos13@gmail.com>
2024-01-21 13:25:17 +0800

    minor updates on updater algo

--- temp/updater.py
+++ temp/updater.py
@@ -8,7 +8,7 @@ class UPDATE_DATA:
     Periodically updates the data through github actions.
     """
     def __init__(self) -> None:
-        pass
+        self.logger = open(os.path.join("temp","logger.txt"),"a")
 
     def get_historical_prices(self,ticker_symbol, from_date, to_date) -> list:
         client = EodHistoricalData(os.environ["API_KEY"])
@@ -16,37 +16,41 @@ class UPDATE_DATA:
         return resp
     
     def update_data(self):
-        a = open(os.path.join("data.json"),"r")
-        load_json = json.load(a)
-        a.close()
-        updated_json = {}
-        dates = []
-        for data in load_json.values():
-            try:
-                #take not that by default, the data is ascending
-                if data[-1]['date'] not in dates:
-                    dates.append(datetime.strptime((data[-1]['date']), "%Y-%m-%d"))
-            except IndexError:
-                # "Stock is halted,terminated, or inactive."
-                pass
+        try:
+            a = open(os.path.join("temp","data.json"),"r")
+            load_json = json.load(a)
+            a.close()
+            updated_json = {}
+            dates = []
+            for data in load_json.values():
+                try:
+                    #take not that by default, the data is ascending
+                    if data[-1]['date'] not in dates:
+                        dates.append(datetime.strptime((data[-1]['date']), "%Y-%m-%d"))
+                except IndexError:
+                    # "Stock is halted,terminated, or inactive."
+                    pass
 
-        from_when=max(dates) + timedelta(days=1)
-        to_when=datetime.now().date()
+            from_when=max(dates) + timedelta(days=1)
+            to_when=datetime.now().date()
 
-        ####
+            ####
 
-        for data in load_json.items():
-            update = self.get_historical_prices(data[0].strip(),from_date=from_when,to_date=to_when)
-            updated_json[data[0]] = data[1] + update
+            for data in load_json.items():
+                update = self.get_historical_prices(data[0].strip(),from_date=from_when,to_date=to_when)
+                updated_json[data[0]] = data[1] + update
 
-        update_data = open(os.path.join("temp","data.json"),"w")
-        json.dump(updated_json,update_data,indent=2)
+            update_data = open(os.path.join("temp","data.json"),"w")
+            json.dump(updated_json,update_data,indent=2)
             
-        ####
+        #### 
+            self.logger.write(f"Updated --- {datetime.now()}\n")
+            self.logger.close()
 
-        logger = open(os.path.join("temp","logger.txt"),"a")
-        logger.write(f"Updated --- {datetime.now()}\n")
-        logger.close()
+        except Exception as e:
+            self.logger.write(f"########\nError encountered --- {datetime.now()}\nError Message: {e}\n")
+            self.logger.close()
+
 
 
 
